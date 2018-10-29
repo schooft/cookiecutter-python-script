ec27345c1480f60179074d5e4c18b8914baa30f2
Tim Schoof <tim.schoof@desy.de>
2018-10-29 13:37:45 +0100

    Add -u option to make git pull work

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,5 +7,5 @@ git init \
 && git remote add origin git@github.com:{{cookiecutter.github_user}}/{{cookiecutter.project_name}}.git \
 && echo "TODO: Create new repository {{cookiecutter.github_user}}/{{cookiecutter.project_name}} on github at https://github.com/new" \
 && echo "TODO: Activate Travis CI Github App integration for this project" \
-&& echo "TODO: Execute: git push origin master"
+&& echo "TODO: Execute: git push -u origin master"
 
