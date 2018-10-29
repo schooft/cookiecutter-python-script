02d2473ecf22f3322a857a661da7bb910ab61371
Tim Schoof <tim.schoof@desy.de>
2018-10-29 13:09:15 +0100

    Add travis ci TODO

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -6,5 +6,6 @@ git init \
 && git commit -m "Initial setup" \
 && git remote add origin git@github.com:{{cookiecutter.github_user}}/{{cookiecutter.project_name}}.git \
 && echo "TODO: Create new repository {{cookiecutter.github_user}}/{{cookiecutter.project_name}} on github at https://github.com/new" \
-&& echo "TODO: git push origin master"
+&& echo "TODO: Activate Travis CI Github App integration for this project" \
+&& echo "TODO: Execute: git push origin master"
 
