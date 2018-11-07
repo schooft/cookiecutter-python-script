aed14ea06cc18ba09575ee776cbf8995d2363570
Tim Schoof <tim.schoof@desy.de>
2018-11-07 16:39:25 +0100

    Extend example script and tests
    Now the script serves as a short example of how to use argparse and 
    pytest.

--- {{cookiecutter.project_name}}/test_{{cookiecutter.project_name}}.py
+++ {{cookiecutter.project_name}}/test_{{cookiecutter.project_name}}.py
@@ -1,5 +1,39 @@
 import {{cookiecutter.project_name}}
+import pytest
 
 
-def test_main():
-    assert {{cookiecutter.project_name}}.main() == 0
+def test_greeting():
+    assert {{cookiecutter.project_name}}.greeting("John") == "Hello, John!"
+
+
+def test_greeting_en():
+    assert {{cookiecutter.project_name}}.greeting("John", "en") == "Hello, John!"
+
+
+def test_greeting_de():
+    assert {{cookiecutter.project_name}}.greeting("John", "de") == "Hallo, John!"
+
+
+def test_greeting_unknown_lang():
+    with pytest.raises(ValueError):
+        {{cookiecutter.project_name}}.greeting("John", "fr")
+
+
+@pytest.mark.parametrize("argv, lang, name", [
+    (["John"], "en", "John"),
+    (["-l", "en", "John"], "en", "John"),
+    (["-l", "de", "John"], "de", "John"),
+    (["John Doe"], "en", "John Doe"),
+])
+def test_parse_args(argv, lang, name):
+    args = {{cookiecutter.project_name}}.parse_args(argv)
+    assert args.name == name
+    assert args.language == lang
+
+
+def test_main(capsys):
+    ret = {{cookiecutter.project_name}}.main(["-l", "en", "John"])
+    assert ret == 0
+    captured = capsys.readouterr()
+    assert captured[0] == "Hello, John!\n"  # stdout
+    assert captured[1] == ""  # stderr
--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}.py
@@ -2,10 +2,30 @@
     A skeleton python script
 """
 import sys
+import argparse
 
 
-def main():
-    print("Hello World")
+def greeting(name, lang='en'):
+    if lang == "en":
+        return "Hello, {}!".format(name)
+    elif lang == "de":
+        return "Hallo, {}!".format(name)
+    else:
+        raise ValueError("Unknown language: {}".format(lang))
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(description='Personal greeter.')
+    parser.add_argument('name', type=str, help='your name')
+    parser.add_argument('--language', '-l', type=str, default='en',
+                        help='the language')
+
+    return parser.parse_args(argv)
+
+
+def main(argv=sys.argv[1:]):
+    args = parse_args(argv)
+    print(greeting(args.name, args.language))
     return 0
 
 
