b5bdb6d1752c12c0a4e955705ad5a8b436751626
Tim Schoof <tim.schoof@desy.de>
2018-11-07 17:00:41 +0100

    Do not commit immediately to let the user replace the example content

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,10 +2,10 @@
 git init \
 && git config user.name "{{cookiecutter.author_name}}" \
 && git config user.email {{cookiecutter.author_email}} \
-&& git add . \
-&& git commit -m "Initial setup" \
 && git remote add origin git@github.com:{{cookiecutter.github_user}}/{{cookiecutter.project_name}}.git \
 && echo "TODO: Create new repository {{cookiecutter.github_user}}/{{cookiecutter.project_name}} on github at https://github.com/new" \
 && echo "TODO: Activate Travis CI Github App integration for this project" \
+&& echo "TODO: Execute: git add ." \
+&& echo "TODO: Execute: git commit -m \"Initial setup\"" \
 && echo "TODO: Execute: git push -u origin master"
 
